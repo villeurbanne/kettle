3777985f7a90dcb1089da80bd0fcbaa371df0ef8
mathis <54747007+underhoney@users.noreply.github.com>
2023-03-27 17:50:23 +0200

    🎨 cosmetic changes in list action

--- src/action_handler/list_action.rs
+++ src/action_handler/list_action.rs
@@ -4,17 +4,18 @@ use std::path::Path;
 pub fn handle_action(kettle_repo_path: &str) {
     if Path::new(kettle_repo_path).exists() {
         let mut kettles_count = read_dir(kettle_repo_path).unwrap().count();
-
-        println!("");
+        
+        println!("\n💌 You have {} kettle(s) :\n", kettles_count);
         for file in fs::read_dir(kettle_repo_path).unwrap() {
             let unwraped_file = file.unwrap().path();
 
             if Path::new(&unwraped_file.as_path()).is_dir() {
-                println!(" {}", unwraped_file.display());
+                let kettle = unwraped_file.to_str().unwrap().split('/');
+                println!(" -> {}", kettle.last().unwrap());
             } else {
                 kettles_count -= 1;
             }
         }
-        println!("\n -> 💌 You have {} kettle(s)", kettles_count);
+        println!("");
     }
 }
