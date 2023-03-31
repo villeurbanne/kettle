7c00970ca9188f2258f687e4af4eaa15d1dd70ce
Saravenpi <saravenpi@tuta.io>
2023-03-31 13:28:56 +0200

    edit: changed list emoji

--- src/action_handler/list_action.rs
+++ src/action_handler/list_action.rs
@@ -5,7 +5,7 @@ pub fn handle_action(kettle_repo_path: &str) {
     if Path::new(kettle_repo_path).exists() {
         let mut kettles_count = read_dir(kettle_repo_path).unwrap().count();
         
-        println!("\n💌 You have {} kettle(s) :\n", kettles_count);
+        println!("\n🫖 You have {} kettle(s) :\n", kettles_count);
         for file in fs::read_dir(kettle_repo_path).unwrap() {
             let unwraped_file = file.unwrap().path();
 
